6742e9b1e7ec4ab7767cd3960d3d4f44aee993ef
tjleejava <tjleejava@gmail.com>
2022-06-25 17:16:52 +0900

    admin 메인페이지 작업 잠시 중단 및 회원관리 페이지 작업

--- today/client/src/pages/Main.js
+++ today/client/src/pages/Main.js
@@ -1,25 +1,25 @@
-import Banner from "../components/mainpage/Banner";
-import MainLayout from "../components/mainpage/MainLayout";
-import MainRanking from "../components/mainpage/MainRanking";
-import MainSearch from "../components/mainpage/MainSearch";
-import MainUser from "../components/mainpage/MainUser";
-import MainCSS from './Main.module.css';
+// import Banner from "../components/mainpage/Banner";
+// import MainLayout from "../components/mainpage/MainLayout";
+// import MainRanking from "../components/mainpage/MainRanking";
+// import MainSearch from "../components/mainpage/MainSearch";
+// import MainUser from "../components/mainpage/MainUser";
+// import MainCSS from './Main.module.css';
 
 function Main() {
 
-    return (
-        <div className={ MainCSS.area }>
-            <div className={ MainCSS.toparea}>
-                <Banner/>
-                <MainUser/>
-            </div>
-            <div className={ MainCSS.botarea }>
-                <MainSearch/>
-                <MainRanking/>
-                <MainLayout/>
-            </div>
-        </div>
-    );
+    // return (
+    //     <div className={ MainCSS.area }>
+    //         <div className={ MainCSS.toparea}>
+    //             <Banner/>
+    //             <MainUser/>
+    //         </div>
+    //         <div className={ MainCSS.botarea }>
+    //             <MainSearch/>
+    //             <MainRanking/>
+    //             <MainLayout/>
+    //         </div>
+    //     </div>
+    // );
 }
 
 export default Main;

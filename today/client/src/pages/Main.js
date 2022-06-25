84d9d1496e70e3c54a53425e9666d206572662ce
DESKTOP-ONN8F4O\h99ww <h99www@gmail.com>
2022-06-25 16:40:14 +0900

    [DESIGN] 메인페이지 컴포넌트 작성

--- today/client/src/pages/Main.js
+++ today/client/src/pages/Main.js
@@ -1,8 +1,23 @@
+import Banner from "../components/mainpage/Banner";
+import MainLayout from "../components/mainpage/MainLayout";
+import MainRanking from "../components/mainpage/MainRanking";
+import MainSearch from "../components/mainpage/MainSearch";
+import MainUser from "../components/mainpage/MainUser";
+import MainCSS from './Main.module.css';
+
 function Main() {
 
     return (
-        <div>
-            <h1>메인화면</h1>
+        <div className={ MainCSS.area }>
+            <div className={ MainCSS.toparea}>
+                <Banner/>
+                <MainUser/>
+            </div>
+            <div className={ MainCSS.botarea }>
+                <MainSearch/>
+                <MainRanking/>
+                <MainLayout/>
+            </div>
         </div>
     );
 }

ba1c9d9b117ea143fa992704343533c69e9a9878
DESKTOP-ONN8F4O\h99ww <h99www@gmail.com>
2022-07-20 16:27:04 +0900

    [FEAT] 관리자 문의관리

--- today/client/src/components/platform-inquiry/search-form/InquiryList.js
+++ today/client/src/components/platform-inquiry/search-form/InquiryList.js
@@ -10,13 +10,33 @@ function InquiryList({inquiry}) {
     navigate(`/admin/inquiries/${no}`);
   };
 
+  const onMouseOverHandler = (no) => {
+    const tr = document.getElementById(no);
+    tr.style.backgroundColor='black';
+    tr.style.color='white';
+    tr.style.cursor='pointer';
+  };
+
+  const onMouseOutHandler = (no) => {
+    const tr = document.getElementById(no);
+    tr.style.backgroundColor='white';
+    tr.style.color='black';
+  };
+
   return(
-    <tr onClick={ () => inquiryOnClickHandler(inquiry.platformInquiryNo) }  key={inquiry.platformInquiryNo}>
+    <tr 
+      className={ InquiryListCSS.tds}
+      onClick={ () => inquiryOnClickHandler(inquiry.platformInquiryNo) }  
+      key={inquiry.platformInquiryNo}
+      onMouseOver={ () => onMouseOverHandler(inquiry.platformInquiryNo)}
+      onMouseOut={ () => onMouseOutHandler(inquiry.platformInquiryNo)}
+      id={inquiry.platformInquiryNo}
+    >
       <td>{inquiry.platformInquiryNo}</td>
+      <td>{inquiry.replyYN =='Y' ? '완료': '대기'}</td>
       <td>{inquiry.platformInquiryTitle}</td>
-      <td>{inquiry.platformInquiryDate}</td>
       <td>{inquiry.nickname}</td>
-      <td>{inquiry.replyYN}</td>
+      <td>{inquiry.platformInquiryDate}</td>
     </tr>
   );
 };
--- today/client/src/pages/platformqna/AdminInquiry.js
+++ today/client/src/pages/platformqna/AdminInquiry.js
@@ -1,3 +1,4 @@
+import AdminInquiryCSS from './AdminInquiry.module.css';
 import { useSelector } from "react-redux";
 import InquiryList from "../../components/platform-inquiry/search-form/InquiryList";
 import SearchForm from "../../components/platform-inquiry/search-form/SearchForm";
@@ -5,11 +6,30 @@ import SearchForm from "../../components/platform-inquiry/search-form/SearchForm
 function AdminInquiry() {
   const { inquiries } = useSelector(state => state.platformQnaReducer);
   return (
-    <div>
-      <h1>관리자 문의 관리 페이지</h1>
-      <table>
-        { inquiries.map( inquiry => <InquiryList inquiry={inquiry}/>)}
-      </table>
+    <div className={AdminInquiryCSS.area}>
+      <div className={AdminInquiryCSS.head}>
+        <h1>문의 관리</h1>
+        <hr/>
+      </div>
+      <div className={AdminInquiryCSS.body}>
+        <table>
+          <colgroup>
+              <col width = "10%"/>
+              <col width = "15%"/>
+              <col width = "35%"/>
+              <col width = "25%"/>
+              <col width = "15%"/>
+          </colgroup>
+          <thead className={ AdminInquiryCSS.tablehead }>
+            <th>문의 번호</th>
+            <th>답변 상태</th>
+            <th>문의 제목</th>
+            <th>작성자</th>
+            <th>문의 날짜</th>
+          </thead>
+          { inquiries.map( inquiry => <InquiryList inquiry={inquiry}/>)}
+        </table>
+      </div>
       <SearchForm/>
     </div>
   );

2de0e296032e75ac4a8617b3332ceda0181bdc99
seoyeome <seoyeomedev@gmail.com>
2022-07-25 02:27:25 +0900

    [STYLE] 뒤로가기, 수정하기 버튼 추가

--- today/client/src/components/challenge/inquiry/InquiryContent.js
+++ today/client/src/components/challenge/inquiry/InquiryContent.js
@@ -1,15 +1,18 @@
 import InquiryDetailCSS from './InquiryDetailCSS.module.css';
 import { useLocation } from "react-router-dom";
+import { Link, useNavigate } from 'react-router-dom';
 
 function InquiryContent() {
 
   const { state } = useLocation();
+  const navigate = useNavigate();
+
   console.log(state);
   const writer = state.nickname;
   const registedDate = state.challengeInquiryDate;
   const content = state.challengeInquiryContent;
   const title = state.challengeInquiryTitle;
-  
+
   return(
     <div className={InquiryDetailCSS.container}>
       <div className={InquiryDetailCSS.header}>
@@ -25,6 +28,10 @@ function InquiryContent() {
       </div>
       <div className={InquiryDetailCSS.contentBox}>
         {content}
+      </div>
+      <div className={InquiryDetailCSS.btnArea}>
+        <button onClick={() => {navigate(-1)} }className={InquiryDetailCSS.btn}>뒤로가기</button>
+        <button className={InquiryDetailCSS.btn}>수정</button>
       </div>
     </div>
   )

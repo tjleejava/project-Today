9b903c47800cca70875d898781357f216474fdb9
seoyeome <seoyeomedev@gmail.com>
2022-07-25 02:35:20 +0900

    [STYLE] 삭제 버튼 추가

--- today/client/src/components/challenge/inquiry/InquiryContent.js
+++ today/client/src/components/challenge/inquiry/InquiryContent.js
@@ -1,6 +1,8 @@
 import InquiryDetailCSS from './InquiryDetailCSS.module.css';
 import { useLocation } from "react-router-dom";
 import { Link, useNavigate } from 'react-router-dom';
+import {Cookies} from 'react-cookie'
+import jwt_decode from "jwt-decode";
 
 function InquiryContent() {
 
@@ -12,6 +14,10 @@ function InquiryContent() {
   const registedDate = state.challengeInquiryDate;
   const content = state.challengeInquiryContent;
   const title = state.challengeInquiryTitle;
+  const cookies = new Cookies();
+  const token = cookies.get('token');
+  const decoded = jwt_decode(token);
+  const memberNo = decoded.no;
 
   return(
     <div className={InquiryDetailCSS.container}>
@@ -32,6 +38,7 @@ function InquiryContent() {
       <div className={InquiryDetailCSS.btnArea}>
         <button onClick={() => {navigate(-1)} }className={InquiryDetailCSS.btn}>뒤로가기</button>
         <button className={InquiryDetailCSS.btn}>수정</button>
+        {(memberNo == state.memberNo)?<button className={InquiryDetailCSS.deleteBtn}>삭제</button>: null}
       </div>
     </div>
   )
